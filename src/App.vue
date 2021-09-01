38024830369ef3a022648a34a2f6ffa8d34182f0
pengxueyou <pengxueyou@hikvision.com.cn>
2021-09-01 18:46:04 +0800

    fix: 重构项目

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,26 @@
 <template>
-	<div id="app">
-		<router-view />
-	</div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
+
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: "App",
-  data() {
-    return {
-      mainRoute: "/Main/Home"
-    };
-  },
-  mounted() {
-    if (this.$route.fullPath.indexOf("/Main") !== -1) {
-      this.mainRoute = this.$route.fullPath;
-    }
-  },
-  watch: {
-    "$route.path": function(newVal, oldVal) {
-      console.log(newVal);
-      // 添加门禁控制的默认路径
-      if (newVal === "/Main") {
-        this.$router.push(this.mainRoute);
-      }
-    }
+  name: 'App',
+  components: {
+    HelloWorld
   }
-};
+}
 </script>
+
 <style>
 #app {
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	height: 100%;
-	min-width: 1440px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
